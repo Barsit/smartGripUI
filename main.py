beb9c8534bd21310e615e439f13e3e6179988271
“Cabbagebirdwww” <1653998065@qq.com>
2022-10-20 21:53:00 +0800

    zhushi

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Stats:
 
     def __init__(self):
         # 从文件中加载UI定义
-
+#hhhh
         # 从 UI 定义中动态 创建一个相应的窗口对象
         # 注意：里面的控件对象也成为窗口对象的属性了
         # 比如 self.ui.button , self.ui.textEdit
